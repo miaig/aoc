bb862211e4e76f21ae2a4601a3dddc5b4333af24
miaig <lorenz@kieler.cc>
2024-12-13 11:23:34 +0100

    day13-2

--- day13/day13.py
+++ day13/day13.py
@@ -40,26 +40,27 @@ def calc(valxA, valxB, valyA, valyB, resX, resY):
     eq2 = sp.Eq(x*valyA + y*valyB, resY)
     ans = sp.solve((eq1, eq2), (x, y))
     # tokens =nsns[0]*3 + ans[1]
-    # print(ans) # {x: 80, y: 40}
+    print(ans) # {x: 80, y: 40}
     # print(type(ans[x])) # 80
     # print(ans[y]) # 40
     # tokens = ans[x]*3 + ans[y]
 
-    if type(ans[x]) and type(ans[x]) == sp.core.numbers.Integer:
+    if (type(ans[x]) == sp.core.numbers.Integer) and (type(ans[y]) == sp.core.numbers.Integer):
         # print("hi")
         tokens += ans[x]*3 + ans[y]
     else:
         pass
-    # print("Tokens: ", tokens, "\n")
+    print("Tokens: ", tokens, type(tokens),"\n",) 
 
 i = 0
 while i < len(numbers):
     valxA = numbers[i+0]
     valyA = numbers[i+1]
-    valxB =  numbers[i+2]
+    valxB = numbers[i+2]
     valyB = numbers[i+3]
-    resX =  numbers[i+4]
-    resY = numbers[i+5]
+    resX = numbers[i+4] + 10000000000000
+    resY = numbers[i+5] + 10000000000000
+    print(i)
     calc(valxA, valxB, valyA, valyB, resX, resY)
     # print(valxA, valxB, valyA, valyB, resX, resY)
     i += 6
@@ -68,3 +69,4 @@ while i < len(numbers):
 # print("".join(fileinput))
 
 print("\n\n", tokens)
+print(type(tokens))
